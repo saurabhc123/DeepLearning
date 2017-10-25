82533ef340271d7d414eb064e3c7425c2e209b62
Saurabh Chakravarty <saurabc@yahoo.com>
2017-10-25 01:09:23 -0400

    Added new RNN code.

--- Notebooks/Assignment-2/RNN_Vanilla-1.py
+++ Notebooks/Assignment-2/RNN_Vanilla-1.py
@@ -1,82 +1,108 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import csv
+import numpy as np
 
 DATA_DIR = 'MNIST'
 sentiment_data = 'sentiment-data'
 
 
-reader = csv.reader(open(sentiment_data +'/word-vectors.txt'))
+def getWordVectorDict():
+    reader = csv.reader(open(sentiment_data +'/word-vectors-refine.txt'))
 
-word_vector_dict = {}
-for row in reader:
-    key = row[0]
-    if key in word_vector_dict:
-        # implement your duplicate row handling here
-        pass
-    word_vector_dict[key] = row[1:]
-print word_vector_dict['agassi']
+    word_vector_dict = {}
+    for row in reader:
+        key = row[0]
+        if key in word_vector_dict:
+            # implement your duplicate row handling here
+            pass
+        word_vector_dict[key] = np.array(row[1:])
+    return word_vector_dict
+
+def getPaddedSentenceMatrix(sentenceMatrix):
+    wordCount = 100
+    return np.vstack((sentenceMatrix, np.zeros((wordCount - np.shape(sentenceMatrix)[0],np.shape(sentenceMatrix)[1]), dtype=np.float32)))
+
+def getVectorForSentence(sentence, word_vec_dict):
+    sentence_matrix = []
+    for word in sentence.split(' '):
+        word_vec = word_vec_dict[word]
+        if(len(sentence_matrix) == 0):
+            sentence_matrix = word_vec
+        else:
+            sentence_matrix = np.vstack((sentence_matrix,word_vec))
+    return getPaddedSentenceMatrix(sentence_matrix)
 
 
-sent1 = "agassi comes"
-sent2 = "agassi"
-sent1vec = []
-sent2vec = []
+#sm = np.ones((70,50), dtype=np.float32);
+#psm = getPaddedSentenceMatrix(sm)
+#print psm
+#word_vec_d = getWordVectorDict()
+#sm = getVectorForSentence("as said that in for on", word_vec_d)
+#print sm
 
-for word in sent1.split(' '):
-    sent1vec.append(word_vector_dict[word])
+def getData(fileName):
+    reader = csv.reader(open(sentiment_data +'/' + fileName))
+    trainingData = []
+    for row in reader:
+        data = {}
+        data['label'] =  1 if row[0] == 'postive' else 0
+        data['sentence'] = row[1:]
+        trainingData.append(data)
+    return trainingData
 
-for word in sent2.split(' '):
-    sent2vec.append(word_vector_dict[word])
+word_vec_dict = getWordVectorDict()
 
-batched_data = tf.train.batch(
-    tensors=[sent1vec],
-    batch_size=55,
-    dynamic_pad=True,
-    name="y_batch"
-)
 
-mnist = input_data.read_data_sets(DATA_DIR, one_hot=True)
+def transform(row):
+    return row['label'], getVectorForSentence(row['sentence'][0], word_vec_dict)
 
-element_size = 28;
-time_steps = 28;
-num_classes = 10
-batch_size = 128;
-n_iterations = 3000;
-hidden_layer_size = 128
+
+word_vector_size = 50;
+time_steps = 100;
+num_classes = 2
+batch_size = 2;
+n_iterations = 50;
+hidden_layer_size = 50
+
+training_data = getData('train.csv')
+training_rows  = map(lambda row: transform(row), training_data)
+training_data = map(lambda row: row[1], training_rows)
+training_labels = map(lambda row: row[0], training_rows)
+#test_data = map(lambda row: transform(row), getData('test.csv'))
+test_data = getData('test.csv')
+test_rows  = map(lambda row: transform(row), test_data)
+test_data = map(lambda row: row[1], test_rows)
+test_labels = map(lambda row: row[0], test_rows)
+print training_data[0]
+
+#mnist = input_data.read_data_sets(DATA_DIR, one_hot=True)
+
+
 
 #Setting up the input and labels placeholders
-_inputs = tf.placeholder(tf.float32,shape=[None, time_steps,
-                                           element_size],
-                                           name='inputs')
-y = tf.placeholder(tf.float32, shape=[None, num_classes],name='inputs')
+_inputs = tf.placeholder(tf.float32, shape=[None, time_steps,
+                                            word_vector_size],
+                         name='inputs')
+y = tf.placeholder(tf.int32, shape=[None],name='inputs')
+y_one_hot = tf.one_hot( y , num_classes )
 
 # TensorFlow built-in functions
 # Creating the RNN cell and creating the outputs
-rnn_cell = tf.contrib.rnn.BasicRNNCell(hidden_layer_size)
+rnn_cell = tf.contrib.rnn.BasicRNNCell(hidden_layer_size, activation= tf.tanh)
 outputs, states = tf.nn.dynamic_rnn(rnn_cell, _inputs, dtype=tf.float32)
-
-Wl = tf.Variable(tf.truncated_normal([hidden_layer_size, num_classes],
-                                     mean=0,stddev=.01))
-bl = tf.Variable(tf.truncated_normal([num_classes],mean=0,stddev=.01))
-
-def get_linear_layer(vector):
-    return tf.matmul(vector, Wl) + bl
-
-last_rnn_output = outputs[:,-1,:]
-#final_output = get_linear_layer(last_rnn_output)
 
 #Create the network
 final_output = tf.layers.dense(states, num_classes)
-softmax = tf.nn.softmax_cross_entropy_with_logits(logits=final_output,labels=y)
+softmax = tf.nn.softmax_cross_entropy_with_logits(logits=final_output,labels=y_one_hot)
 
 #Cross entropy and the optimizer + minimizer
 cross_entropy = tf.reduce_mean(softmax)
-train_step = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cross_entropy)
-#train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+train_step = tf.train.RMSPropOptimizer(0.1, 0.9, 0.01).minimize(cross_entropy)
+#train_step = tf.train.AdamOptimizer(0.1).minimize(cross_entropy)
 
 #Metrics
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(final_output,1))
+correct_prediction = tf.equal(tf.argmax(y_one_hot,1), tf.argmax(final_output,1))
 accuracy = (tf.reduce_mean(tf.cast(correct_prediction, tf.float32)))*100
 
 #Initialize session
@@ -84,24 +110,29 @@ sess=tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 
 #Initialize test data
-test_data = mnist.test.images[:batch_size].reshape((-1,
-                                            time_steps, element_size))
-test_label = mnist.test.labels[:batch_size]
+#test_data = mnist.test.images[:batch_size].reshape((-1,time_steps, word_vector_size))
+#test_label = mnist.test.labels[:batch_size]
 
 for i in range(n_iterations):
-
-       batch_x, batch_y = mnist.train.next_batch(batch_size)
-       batch_x = batch_x.reshape((batch_size, time_steps, element_size))
-       sess.run(train_step,feed_dict={_inputs:batch_x,
-                                      y:batch_y})
-       if i % 100 == 0:
+    for j in range(len(training_data)/batch_size):
+        print "j:", j
+        startIndex = j*batch_size
+        endIndex = startIndex + batch_size
+        batch_x = np.array(training_data[startIndex : endIndex]).reshape((-1,time_steps, word_vector_size))
+        batch_y = np.array(training_labels[startIndex : endIndex]).reshape(batch_size)
+        #batch_x, batch_y = mnist.train.next_batch(batch_size)
+        #batch_x = batch_x.reshape((batch_size, time_steps, word_vector_size))
+        sess.run(train_step, feed_dict={_inputs: batch_x,
+                                        y: batch_y})
+        if i % 1 == 0:
             acc = sess.run(accuracy, feed_dict={_inputs: batch_x, y: batch_y})
-            loss = sess.run(cross_entropy,feed_dict={_inputs:batch_x,y:batch_y})
+            loss = sess.run(cross_entropy, feed_dict={_inputs: batch_x, y: batch_y})
             print ("Iter " + str(i) + ", Minibatch Loss= " + \
-					  "{:.6f}".format(loss) + ", Training Accuracy= " + \
-					  "{:.5f}".format(acc))
+                   "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                   "{:.5f}".format(acc))
+
 
 print ("Testing Accuracy:",
-    sess.run(accuracy, feed_dict={_inputs: test_data, y: test_label}))
+    sess.run(accuracy, feed_dict={_inputs: test_data, y: test_labels}))
 
 
